da9eb3e460eeabaac95bee53ca282f9855e8980a
Niclas Lovdahl <niclas.lovdahl@gmail.com>
2014-09-18 00:55:12 +0200

    Changed extended arraylist to integrated arraylist

---
+++ EDA061/src/Program/Program.java
@@ -0,0 +1,18 @@
+package Program;
+
+import java.util.ArrayList;
+import Instructions.Instruction;
+
+public class Program {
+
+	public ArrayList<Instruction> list;
+
+	public Program() {
+
+	}
+	
+	public void add(Instruction i){
+		list.add(i);
+	}
+
+}

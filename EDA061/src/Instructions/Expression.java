c9dcddcd85614a03fbb96d077936d4400bb29a03
Niclas Lovdahl <niclas.lovdahl@gmail.com>
2014-09-22 14:43:19 +0200

    Expression added

---
+++ EDA061/src/Instructions/Expression.java
@@ -0,0 +1,15 @@
+package Instructions;
+
+import Computer.Memory;
+
+public abstract class Expression implements Instruction {
+
+	public Expression() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void execute(Memory mem){
+		
+	}
+
+}
